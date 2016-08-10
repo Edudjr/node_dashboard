c2348740a76c7837f98bf2e6e291b83dbb392c36
Eduardo Domene Jr <eduardo.djr@hotmail.com>
2016-08-10 01:51:59 -0300

    Added local bootstrap and jquery

--- public/js/source.js
+++ public/js/source.js
@@ -1,1 +1,7 @@
-console.log("OK");
+var dateFormat = require('dateformat');
+var now = new Date();
+
+window.onload = function(){
+	// Basic usage 
+	console.log(dateFormat(now, "dddd, mmmm dS, yyyy, h:MM:ss TT"));
+}

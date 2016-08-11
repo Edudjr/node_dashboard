44606311d5a17a772c9b5c0115a22729a58aa8a8
Eduardo Domene Jr <eduardo.djr@hotmail.com>
2016-08-11 01:07:58 -0300

    Removed time log. Added endpoints for back play and next

--- public/js/source.js
+++ public/js/source.js
@@ -4,14 +4,11 @@ function updateClock(){
 	var clockLabel = document.getElementById('div_hour');
 	var p = clockLabel.getElementsByTagName('p')[0];
 	var now = new Date();
-	p.innerHTML = dateFormat(now, "h:MM:ss");
-
-	console.log(clockLabel);
-	console.log(p);
+	p.innerHTML = dateFormat(now, "h:MM");
 }
 
 window.onload = function(){
 	updateClock();
 
-	setInterval(updateClock, 1000);
+	setInterval(updateClock, 60000);
 }
--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,18 @@ router.get('/', function(req, res, next) {
   //res.sendFile(path.join(__dirname+'/../views/index.html'));
 });
 
+router.get('/back', function(req, res, next) {
+	console.log('BACK');
+});
+
+router.get('/play', function(req, res, next) {
+  res.render('index', {title:'Teste'});
+  //res.sendFile(path.join(__dirname+'/../views/index.html'));
+});
+
+router.get('/next', function(req, res, next) {
+  res.render('index', {title:'Teste'});
+  //res.sendFile(path.join(__dirname+'/../views/index.html'));
+});
+
 module.exports = router;

29c5cdf6341ee56686dc5bc1e4df8b3f65d60eeb
Eduardo Domene Jr <eduardo.djr@hotmail.com>
2016-08-13 13:16:26 -0300

    added player module and music sample

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,20 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+var Player = require('player');
+//var fs = require('fs');
+//var folderPath = path.join(__dirname, '/../music/');
+ 
+// create a player instance from playlist 
+//var player = Player([]);
+
+// Read folder files
+// fs.readdir(folderPath, function (err, files) {
+//     if (err) { return cb(err, null); }
+//     //cb(null, files);
+//     console.log(files);
+// });
+//player.add(path.join(__dirname,'/../music/'));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -13,13 +27,14 @@ router.get('/back', function(req, res, next) {
 });
 
 router.get('/play', function(req, res, next) {
-  res.render('index', {title:'Teste'});
-  //res.sendFile(path.join(__dirname+'/../views/index.html'));
+  	// play now and callback when playend 
+	// player.play(function(err, player){
+	//   console.log('playend!');
+	// });
 });
 
 router.get('/next', function(req, res, next) {
-  res.render('index', {title:'Teste'});
-  //res.sendFile(path.join(__dirname+'/../views/index.html'));
+	player.next();
 });
 
 module.exports = router;
